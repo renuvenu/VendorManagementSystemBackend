f318ee87eacec9ef3a5ffde7c27f35e08522df76
Github Pooviga <Github spooviga01@gmail.com>
2023-07-11 15:09:47 +0530

    added productpurchaseorder

--- Model/ProductPurchaseOrder.cs
+++ Model/ProductPurchaseOrder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,15 @@ namespace Model
 {
     public class ProductPurchaseOrder
     {
+        [Required]
         public Guid Id { get; set; }
- 
+        [Required]
         public Guid PurchaseOrderId { get; set; }
 
+        [Required]
         public Guid ProductId { get; set; }
 
+        [Required]
         public long Quantity { get; set; }
     }
 }
--- VendorManagement_WebApi/Controllers/ProductPurchaseOrderController.cs
+++ VendorManagement_WebApi/Controllers/ProductPurchaseOrderController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Model.Requests;
+using Model;
 using Repository;
 
 namespace VendorManagement_WebApi.Controllers
@@ -12,6 +15,44 @@ namespace VendorManagement_WebApi.Controllers
         public ProductPurchaseOrderController(DbContextAccess productpurchasedetailDBContext1)
         {
             this.productpurchasedetailDBContext1 = productpurchasedetailDBContext1;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetProductPurchaseDetail()
+        {
+            return Ok(await productpurchasedetailDBContext1.productpurchaseorder.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertProductPurchaseOrder(InsertProductPurchaseRequest insertProductPurchaseRequest)
+        {
+
+            if (insertProductPurchaseRequest != null)
+            {
+                ProductPurchaseOrder productpurchasedetail = new ProductPurchaseOrder();
+
+                productpurchasedetail.Id = new Guid();
+                productpurchasedetail.PurchaseOrderId = insertProductPurchaseRequest.PurchaseOrderId;
+                productpurchasedetail.ProductId = insertProductPurchaseRequest.ProductId;
+                productpurchasedetail.Quantity = insertProductPurchaseRequest.Quantity;
+
+                await productpurchasedetailDBContext1.productpurchaseorder.AddAsync(productpurchasedetail);
+                await productpurchasedetailDBContext1.SaveChangesAsync();
+
+                return Ok(productpurchasedetail);
+            }
+            else
+            {
+                return BadRequest("Product Detail is not available");
+            }
+        }
+        [HttpGet]
+
+        [Route("{Id}")]
+        public async Task<IActionResult> GetElementById([FromRoute] Guid Id)
+        {
+            return await Task.FromResult<IActionResult>(Ok(productpurchasedetailDBContext1.productpurchaseorder.Where(x => x.Id.Equals(Id))));
         }
     }
 }
