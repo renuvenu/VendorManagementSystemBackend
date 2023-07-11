511be3862378b2e42a8294db011d5fffba73a47d
Keerthana0505 <keerthanajothi2002@gmail.com>
2023-07-11 21:17:05 +0530

    Added Controller Methods for VendorDetails

---
+++ Model/Requests/VendorDetailswithProductDetailsRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Requests
+{
+    public class VendorDetailswithProductDetailsRequest
+    {
+        public VendorDetails VendorDetails { get; set; }
+        public List<ProductDetail> ProductDetails { get; set; }
+    }
+}
